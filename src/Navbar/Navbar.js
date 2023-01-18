f1d9e6338cb86b53be0340352be646a661d69cc7
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-01-18 15:45:08 +0300

    edited nav

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Navbar.css'
-// import HomeIcon from '@mui/icons-material/Home';
+import { FaHome } from 'react-icons/fa';
+import {FaMoon} from 'react-icons/fa'
+import {BsCupStraw} from 'react-icons/bs'
+import {MdNoFood} from 'react-icons/md'
+
+
 
 function Navbar() {
   return (
     <nav className='nav'>
       <div className='sidenav'>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
+        <p><FaHome/></p>
+        <p><BsCupStraw/></p>
+        <p><MdNoFood/></p>
+        <p><FaMoon/></p>
       </div>
 
     </nav>
