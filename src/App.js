4ac3f72cb3fa3a620a576d2a6af8af058b3e8df0
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-01-20 05:40:14 +0300

    mode component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Homedesert from './Burgers/Homedesert'
 import Homecoktail from './NutritionF/Homecoktail'
 import Footer from './Footer/Footer'
@@ -7,22 +7,33 @@ import Beverege from './Beverages/Beverege'
 import './App.css'
 import {BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import { Fragment } from 'react'
+import { createTheme, ThemeProvider } from '@mui/material'
+
 
 
 
 function App() {
+  const[mode,setMode]=useState('light')
+
+  const darkTheme=createTheme({
+    palette:{
+      mode:mode
+    }
+  })
   return (
-    <Router>
-      <Fragment>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Homecoktail/>}  />
-          <Route path='/burgers' element={<Homedesert/>}  />
-          <Route path='/drinks' element={<Beverege/>}  />
-        </Routes>
-        <Footer/>
-      </Fragment>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Fragment>
+          <Navbar mode={mode} setMode={setMode}/>
+          <Routes>
+            <Route path='/' element={<Homecoktail/>}  />
+            <Route path='/burgers' element={<Homedesert/>}  />
+            <Route path='/drinks' element={<Beverege/>}  />
+          </Routes>
+          <Footer/>
+        </Fragment>
+      </Router>
+    </ThemeProvider>
   )
 }
 
