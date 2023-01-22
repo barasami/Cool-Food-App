1386b929d02752aa7ccc80b87642b81def090cb3
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-01-22 09:25:38 +0300

    loading comp

--- src/Burgers/Homedesert.js
+++ src/Burgers/Homedesert.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import {deSert} from './burger'
 import './Burger.css'
 import { Paper,Typography } from '@mui/material'
-import CircularProgress from '@mui/material/CircularProgress';
+import { CircularProgress } from '@mui/material'
 
 
 
@@ -22,26 +22,27 @@ function Homedesert() {
 
   return (
     <>
-     {allDesset.map((myburger)=>{
-      const{desc,name,images,ingredients}=myburger
-      let small=images[0].sm
-      let ingname=ingredients[1].name
-      let ingimage=ingredients[0].img
-      return(
-        <>
-          {loading ? <CircularProgress/> :
-            <Paper elevation={2} className='burger'>
-              <Typography variant='h5' color='#00a152' padding={1}>{name}</Typography>
-              <Typography marginLeft={3.3} padding={1}>{desc}</Typography>
-              <img src={small} alt={name} className='image'/>
-              <Typography color='secondary' variant='h6' marginLeft={3}>Ingredients</Typography>
-              <img  src={ingimage} alt={name} />
-              <Typography padding={2}>{ingname}</Typography>
-            </Paper>
-          }
-        </>
-      )
-     })}
+      {loading ? <CircularProgress className='progress' color="success" /> : 
+      <div>
+        {allDesset.map((myburger)=>{
+          const{desc,name,images,ingredients}=myburger
+          let small=images[0].sm
+          let ingname=ingredients[1].name
+          let ingimage=ingredients[0].img
+          return(
+            <>
+              <Paper elevation={2} className='burger'>
+                <Typography variant='h5' color='#00a152' padding={1}>{name}</Typography>
+                <Typography marginLeft={3.3} padding={1}>{desc}</Typography>
+                <img src={small} alt={name} className='image'/>
+                <Typography color='secondary' variant='h6' marginLeft={3}>Ingredients</Typography>
+                <img  src={ingimage} alt={name} />
+                <Typography padding={2}>{ingname}</Typography>
+              </Paper>
+            </>
+          )
+        })}
+     </div>}
     </>
   )
 }
