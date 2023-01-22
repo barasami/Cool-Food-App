2013657e70da5e1c04418c76c23375c7fab1b433
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-01-22 08:46:30 +0300

    circular api

--- src/Beverages/Beverege.js
+++ src/Beverages/Beverege.js
@@ -2,15 +2,35 @@ import React, { useEffect, useState } from 'react'
 import { allBeverage } from './beverage'
 import './Beverage.css'
 import { Paper,Typography } from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress';
+
+
+const day=new Date()
+const myday=day.getDate()
+const myMonth=day.getMonth()
+const year=day.getFullYear()
+const minutes=day.getMinutes()
+const hour=day.getHours()
+
+
+const time=`${hour}:${minutes}`
+console.log(myday);
+console.log(day);
+console.log(year);
+console.log(myMonth);
+console.log(time);
 
 
 function Beverege() {
 
     const[beverage,setBeverage]=useState([])
+    const[loading,setLoading]=useState(false)
     useEffect(()=>{
+        setLoading(true)
         allBeverage()
         .then(({data})=>{
             setBeverage(data)
+            setLoading(false)
             console.log(data);
         })
     },[])
@@ -21,11 +41,13 @@ function Beverege() {
         const small=img[0].sm
         return(
           <>
-            <Paper elevation={2} className='beverage'>
-              <p>{name}</p>
-              <img src={small} alt={name}/>
-              <p>{desc}</p>
-            </Paper>
+            {loading ? <CircularProgress className='progress'/> :
+              <Paper elevation={2} className='beverage'>
+                <Typography color='#00a152' padding={1} variant='h5'>{name}</Typography>
+                <img src={small} alt={name}/>
+                <Typography padding={2} marginLeft={1.5}>{desc}</Typography>
+              </Paper>
+            }
           </>
         )
       })}
