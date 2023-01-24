a12519d9e9f790abaaca514fb852c05a720477b3
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-01-24 22:13:23 +0300

    api

--- src/Beverages/Beverege.js
+++ src/Beverages/Beverege.js
@@ -25,14 +25,22 @@ function Beverege() {
     <>
       {loading ? <CircularProgress className='progress' color="success" /> : <div>
       {beverage.map((bev)=>{
-        const{desc,name,img}=bev
+        const{desc,name,img ,ingredients}=bev
         const small=img[0].sm
+        let myname=ingredients[0].name
+        let myimg=ingredients[0].img
+
+      
+        
         return(
           <div key={name}>
             <Paper elevation={2} className='beverage'>
               <Typography color='#00a152' padding={1} variant='h5'>{name}</Typography>
               <img src={small} alt={name} className='image'/>
               <Typography padding={2} marginLeft={1.5}>{desc}</Typography>
+              <Typography variant='h6'>Ingredients</Typography>
+              <Typography>{myname}</Typography>
+              <img src={myimg} alt={myname}/>
             </Paper>
           </div>
         )
