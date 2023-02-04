3853886238c4352d8facf992defe909de3fc993a
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-02-04 21:45:54 +0300

    changed home

--- src/NutritionF/nutrition.js
+++ src/NutritionF/nutrition.js
@@ -1,16 +1,13 @@
 import axios from "axios";
 
-const Url='https://drinks-digital1.p.rapidapi.com/v1/cocktails/search'
+const Url='https://random-recipes.p.rapidapi.com/ai-quotes/3'
 
 const options = {
-    params: {query: 'mojito', limit: '5'},
     headers: {
       'X-RapidAPI-Key': '87f0c01e35msh8962c83c8aef89ap1e9713jsnbf3fe9f2f818',
-      'X-RapidAPI-Host': 'drinks-digital1.p.rapidapi.com'
+      'X-RapidAPI-Host': 'random-recipes.p.rapidapi.com'
     }
 };
-  
-
 
 export const myCooktail=async()=>{
     try {
