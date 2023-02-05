fa606b2bd908a4f273c23b56eadb37a1f061f7b3
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-02-05 22:37:03 +0300

    edited home

--- src/NutritionF/Homecoktail.js
+++ src/NutritionF/Homecoktail.js
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import Form from '../Form/Form'
 import './nutrition.css'
+import { Paper,Typography } from '@mui/material'
+import { CircularProgress } from '@mui/material'
 
 import { myCooktail } from './nutrition'
+
+const mySearch=(value)=>{
+  return value
+}
 
 
 
 function Homecoktail() {
-    const [cooktail,setCooktail]=useState([])
-    const[foodid,setFoodid]=useState(1)
-    useEffect(()=>{
-      myCooktail(foodid)
-        .then(({data})=>{
-            setCooktail(data)
-            console.log(data);
-        })
-    },[foodid])
-
-  const mySearch=(value)=>{
-    value = setFoodid
-    return
-  }
+  const [cooktail,setCooktail]=useState([])
+  const[foodid,setFoodid]=useState(1)
+  useEffect(()=>{
+    myCooktail(foodid)
+      .then(({data})=>{
+        setCooktail(data)
+        console.log(data);
+      })
+  },[foodid])
+ 
   return (
     <>
       <div className='coktail'>
         <Form onSubmitdata={mySearch}/>
+      </div>
+      <div>
+        {cooktail.map((allthings)=>{
+          const{image,title}=allthings
+          return(
+            <>
+              <div>
+                <Typography color='secondary'>{title}</Typography>
+                <img src={image} alt={title}/>
+              </div>
+            </>
+          )
+        })}
       </div>
     </>
   )
