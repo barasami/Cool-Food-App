d6e07e5624e6f45f6f5e96a26bc4f7d2f8815104
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-11 22:24:46 +0300

    modified nav

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import './Navbar.css'
-import { FaHome } from 'react-icons/fa';
-// import {FaMoon} from 'react-icons/fa'
-import {BsCupStraw} from 'react-icons/bs'
-import {MdNoFood} from 'react-icons/md'
 import { Link } from 'react-router-dom';
 import Switch from '@mui/material/Switch';
 
@@ -13,26 +9,14 @@ function Navbar({mode,setMode}) {
   return (
     <nav className='nav'>
       <div className='coolTittle'>
-        <h3 className='title'>
-          Food Hub
-        </h3>
-        <hr className='line'></hr>
+       Spices Hub
       </div>
       <div className='sidenav'>
-        <div className='home'>
-          <Link to='/' className='mlink'><FaHome className='icon'/>Home</Link>
-        </div>
-        <div className='drink'>
-          <Link to='/drinks' className='mlink'><BsCupStraw className='icon'/>Drinks</Link>
-        </div>
-        <div className='food'>
-          <Link to='/burgers' className='mlink'><MdNoFood className='icon'/> Burger</Link>
-        </div>
-        <div className='switch'>
-          <Switch className='switching' defaultChecked color='default' onChange={e=>setMode(mode  === 'light'?'dark' : 'light')}/> Mode
-        </div>
+        <Link to='/' className='mlink'>Home</Link>
+        <Link to='/drinks' className='mlink'>Drinks</Link>
+        <Link to='/burgers' className='mlink'>Burger</Link>
+        <Switch className='switching' defaultChecked color='default' onChange={e=>setMode(mode  === 'light'?'dark' : 'light')}/>
       </div>
-
     </nav>
   )
 }
