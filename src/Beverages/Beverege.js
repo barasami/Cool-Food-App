7bdf38b96e6d3e59dbb93ba35acc573079649dfe
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-14 09:51:48 +0300

    edited bev

--- src/Beverages/Beverege.js
+++ src/Beverages/Beverege.js
@@ -29,10 +29,16 @@ function Beverege() {
         const small=img[0].sm
         let mydrinks=ingredients.map(({id,img,name})=>{
           return(
-            <div key={id}>
-              <img src={img} alt={name} className='image'/>
-              <>{name}</>
-            </div>
+            <table key={id}>
+              <tr>
+                <td>
+                  <img src={img} alt={name} className='image'/>
+                </td>
+              </tr>
+              <tr>
+                <td>{name}</td>
+              </tr>
+            </table>
           )
         })
 
@@ -40,15 +46,15 @@ function Beverege() {
         
         return(
           <div key={name}>
-            <Paper elevation={2} className='beverage'>
-              <Typography color='#00a152' padding={1} variant='h5'>{name}</Typography>
+            <div elevation={2} className='beverage'>
+              <Typography color='#00a152' padding={1} variant='h5' className='text'>{name}</Typography>
               <img src={small} alt={name} className='image'/>
               <Typography padding={2} marginLeft={1.5}>{desc}</Typography>
               <Typography variant='h6' color='secondary' pl={1}>Ingredients</Typography>
               <Typography>
                 {mydrinks}
               </Typography>
-            </Paper>
+            </div>
           </div>
         )
       })}
