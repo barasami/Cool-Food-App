da14e2f84b5ba148379cfb79bb0d05c939fd8ab4
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-18 22:56:57 +0300

    modified nav

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom';
 import Switch from '@mui/material/Switch';
@@ -8,16 +8,24 @@ import ClearIcon from '@mui/icons-material/Clear';
 
 
 function Navbar({mode,setMode}) {
+  const[menu,setMenu]=useState(false)
+  const Openme=()=>{
+    setMenu(!menu)
+  }
   return (
     <nav className='nav'>
       <div className='coolTittle'>
        Spices Hub
       </div>
+
       <div className='sidenav'>
         <Link to='/' className='mlink'>Home</Link>
         <Link to='/drinks' className='mlink'>Drinks</Link>
         <Link to='/burgers' className='mlink'>Burger</Link>
         <Switch className='switching' defaultChecked color='default' onChange={e=>setMode(mode  === 'light'?'dark' : 'light')}/>
+      </div>
+      <div className='icon' onClick={Openme}>
+        { menu ?<DehazeIcon className='show'/> : <ClearIcon className='hide'/> }
       </div>
     </nav>
   )
